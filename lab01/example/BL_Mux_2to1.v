8208a45215bf447e0874205dedd2b44531f884bc
Jeffrey <e24146571@gs.ncku.edu.tw>
2026-08-10 18:15:12 +0800

    add and modify

--- lab01/example/BL_Mux_2to1.v
+++ lab01/example/BL_Mux_2to1.v
@@ -1,9 +1,9 @@
 module BL_Mux_2to1(
-    input [1:0] src,
+    input src_0, src_1,
     input src_sel,
     output channel
 );
 
-    assign channel = (src_sel == 1'b0) ? src[0] : src[1];
+    assign channel = (src_sel == 1'b0) ? src_0 : src_1;
 
 endmodule
